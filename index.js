f157672f25869e2561e7ee42b54002be9f4dff1e
aman <aman.jangra@celebal.tech>
2022-11-30 11:30:41 +0530

    fix(bug): bug fixes

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ console.log("semantic release version control")
 console.log("testing version")
 
 console.log()
+
+console.log("semantic release version control")
+
+console.log("testing version")
+
+console.log()
